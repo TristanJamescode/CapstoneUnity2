3985c55fa2fd7c4807f3f6a67e27b3a5959d1960
Glitchyman <tristanjamesbusiness22@gmail.com>
2023-02-19 13:01:57 -0500

    Door
    press the yellow button to open said door.

---
+++ Assets/code/DoorController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public GameObject Door;
+    public bool doorIsOpening;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(doorIsOpening == true) { 
+        Door.transform.Translate (Vector3.up * Time.deltaTime * 5);
+        }
+        if (Door.transform.position.y > 257)
+        {
+            doorIsOpening = false;  
+        }
+        //thing
+        if (Door.transform.position.y == 257)
+        {
+            doorIsOpening = false;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        doorIsOpening= true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+      {
+            doorIsOpening= true;
+       }
+
+    }
+
+}
