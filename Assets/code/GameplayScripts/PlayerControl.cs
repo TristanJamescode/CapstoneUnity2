cfa9f1238c240ee92cc5ebb209f2162513533165
Curtisthegamedev <curtis.squire@rogers.com>
2023-02-21 14:27:18 -0500

    Fixed Character walking and Jumping Animations

--- Assets/code/GameplayScripts/PlayerControl.cs
+++ Assets/code/GameplayScripts/PlayerControl.cs
@@ -9,115 +9,67 @@ using UnityEngine.Animations;
 /// </summary>
 public class PlayerControl : MonoBehaviour
 {
-    [SerializeField] float moveSpeed_Max = 5;
-    [SerializeField] float moveSpeed = 0.7f;
-    [SerializeField] float Jumpforce = 10;
-    [SerializeField] float GravityS = 1;
-    [SerializeField] GameObject FireParticles; 
-
-    private float moveSpeedCurrent = 0;
-
-    private CharacterController control;
-    private Vector3 Movedir;
-
-    private Animator anim;
-
-    private enum PLAYERSTATE
-    {
-        IDLE,
-        WALK,
-        RUN,
-        JUMP,
-        INAIR,
-        LAND,
-        ATTACK
-    }
-    //private PLAYERSTATE playerState = PLAYERSTATE.IDLE;
-    private bool isGround = true;
-
-    //Inputs
-    private float moveX = 0;
-    private float moveZ = 0;
-    private bool jump = false;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    [SerializeField] private float playerSpeed = 2.0f;
+    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float gravityValue = -9.81f;
+    private Animator Anim; 
 
     private void Start()
     {
-        anim = this.gameObject.GetComponent<Animator>();
-        control = this.gameObject.GetComponent<CharacterController>();
-        FireParticles = GameObject.FindGameObjectWithTag("Fire"); 
-    }
-
-    private void Update()
-    {
-        GetInputs();
-
-        Move();
-
-        ShootFire(); 
-    }
-
-    private void ShootFire()
-    {
-        if(Input.GetButtonDown("Fire1"))
-        {
-            FireParticles.SetActive(true);
-        }
-         
-    }
-    private void GetInputs()
-    {
-        moveX = Input.GetAxisRaw("Horizontal");
-        moveZ = Input.GetAxisRaw("Vertical");
-        jump = Input.GetButtonDown("Jump");
-    }
-
-    private void Move()
-    {
-        isGround = CheckGrounded();
-        //Debug.Log(isGround);
-        if (moveX != 0 || moveZ != 0) //if inputing
-        {
-            if (moveSpeedCurrent < moveSpeed_Max) moveSpeedCurrent += moveSpeed * Time.deltaTime;
-
-            Vector3 direction = new Vector3(moveX, 0f, moveZ).normalized; //direction walk
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f); //rotate Player direction
-            Movedir = (direction * moveSpeedCurrent);
-
-        }
-        else
-        {
-            moveSpeedCurrent = 0;
-            Movedir.x = 0;
-            Movedir.z = 0;
-        }
-        anim.SetFloat("SpeedParam", moveSpeedCurrent / moveSpeed_Max);
-        Movedir.y += (Physics.gravity.y * GravityS); //gravity
-        if (isGround) //if the player is on Ground
-        {
-            Movedir.y = -0.1f;
-            if (jump) Jump();
-            anim.SetBool("IsAir", false);
-        }
-        else
-        {
-            anim.SetBool("IsAir", true);
-        }
-        control.Move(Movedir * Time.deltaTime);
-    }
-
-    private void Jump()
-    {
-        Debug.Log("jump");
-        Movedir.y = Jumpforce;
-        anim.SetTrigger("Jump");
+        controller = this.gameObject.GetComponent<CharacterController>();
+        Anim = this.gameObject.GetComponent<Animator>(); 
     }
 
     private bool CheckGrounded() //Check is on ground with ray, this prevent player can not jump while on tilt ground
     {
-        if (control.isGrounded) { return true; } //This is very odd, sometimes return false even it looks touch the ground
+        if (controller.isGrounded) { return true; } //This is very odd, sometimes return false even it looks touch the ground
         var ray = new Ray(this.transform.position + Vector3.up * 0.1f, Vector3.down);
         var tolerance = 0.3f;
         return Physics.Raycast(ray, tolerance);
+    }
+
+    IEnumerator WaitBeforeJump()
+    {
+        Debug.Log("e function started"); 
+        Anim.SetBool("HasJumped", true); 
+        yield return new WaitForSeconds(0.48f);
+        playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        yield return new WaitForSeconds(1.82f);
+        Anim.SetBool("HasJumped", false); 
+    }
+
+    void Update()
+    {
+        if (CheckGrounded() && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            Anim.SetBool("IsWalking", true);
+        }
+        else if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        {
+            Anim.SetBool("IsWalking", false);
+        }
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && CheckGrounded())
+        {
+            StartCoroutine(WaitBeforeJump()); 
+        }
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
