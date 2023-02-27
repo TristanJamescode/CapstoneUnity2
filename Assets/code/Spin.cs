937a4c316f6a204517326b85b811d342d1586d88
Glitchyman <tristanjamesbusiness22@gmail.com>
2023-02-27 17:07:38 -0500

    More Sounds & modded power ups
    just some small changes atm

---
+++ Assets/code/Spin.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Source:
+//https://www.youtube.com/watch?v=K3Ap2_beGnE
+public class Spin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, 1f, 0f, Space.Self);
+    }
+}
